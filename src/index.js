1bb29f3e2b6b933a50ccada736834804e983e8e1
Khaled <disasterz9955@gmail.com>
2019-03-17 14:52:03 +0300

    Refactoring from fun to class component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return <div>Hi </div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
+    return <div>Hi </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
